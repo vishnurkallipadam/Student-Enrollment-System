31a0dcf5bddb02996379240ff518159b97230c23
Vishnu Mohan V K <vishnumohanvk@gmail.com>
2021-12-15 21:28:47 +0530

    initial commit

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+const bcrypt=require('bcrypt')
+const jwt = require('jsonwebtoken')
+const path = require('path');
+app.use(express.static('./dist/LibraryApp'));
+
+app.use(cors());
+app.use(express.json())
+
+
+
+
+
+app.listen(port,()=>{console.log("server Ready at"+port)});
