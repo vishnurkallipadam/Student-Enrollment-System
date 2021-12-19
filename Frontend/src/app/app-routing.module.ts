d01ab2b6471638fb2c9cbf57f13d40cd32ac1161
SruthiKrishnaR <sruthykrish1122@gmail.com>
2021-12-19 21:32:35 +0530

    Home modify

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseComponent } from './course/course.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',component:HomeComponent},
+                        {path:'course',component:CourseComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
