7febb3744a0761fc3007558e2c5de54c08c8ead7
SruthiKrishnaR <sruthykrish1122@gmail.com>
2021-12-20 20:21:52 +0530

    home page courses

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../course.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  name:any=''
+   details:any=''
+   price:any=''
+   eligibility:any=''
+
+  courses=(this.name,this.details,this.price,this.eligibility)
+
+  constructor(private courseService:CourseService,private router:Router) { }
 
   ngOnInit(): void {
+    this.courseService.getCourses()
+    .subscribe((data)=>{
+      this.courses=JSON.parse(JSON.stringify(data))
+    })
   }
 
 }
